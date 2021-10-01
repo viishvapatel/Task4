c710fc94803699189df2124e5aa4f67537231269
Vishva2002 <vishavishupatel@gmail.com>
2021-10-01 12:05:37 -0400

    code change

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+const taskManager = new TaskManager();
 let validFormFieldInput = (data) =>
 {
     //grabbing the name from the form
@@ -8,20 +9,24 @@ let validFormFieldInput = (data) =>
     //grabbing the description from the form
     const newTaskDescriptionInput = document.querySelector('#newTaskDescriptionInput');
     const description = newTaskDescriptionInput.value;
-    console.log("name:  " + description);
+    console.log("description:  " + description);
 
     //grabbing the AssignedTo from the form
     const newTaskAssignedToInput = document.querySelector
     ("#newTaskAssignedToInput");
     const assignedTo = newTaskAssignedToInput.value;
-    console.log("name:  " + assignedTo);
+    console.log("assignedTo:  " + assignedTo);
 
     //grabbing the Due Date from the form
-    const newTaskDueDateInput = document.querySelector("#newTaskAssignedToInput");
+    const newTaskDueDateInput = document.querySelector("#newTaskDueDateInput");
     const dueDate = newTaskDueDateInput.value;
-    console.log("name:  " + dueDate);
+    console.log("dueDate:  " + dueDate);
+    taskManager.addTask(name, description, assignedTo, dueDate);
+    taskManager.render();
+    taskManager.save();
+    taskManager.render();
 }
-validFormFieldInput();
+
 function myFunction() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
@@ -30,6 +35,44 @@ function myFunction() {
         x.style.display = "none";
     }
 }
-const taskHtml = createTaskHtml("Groceries", "description", "Vishva", "09/27/2021")
 
-console.log(taskHtml);
+tasksList.addEventListener("click", (event) => {
+    if(event.target.classList.contains
+        ('done-button'))
+        {
+        let parentTask = event.target.parentNode;
+        let taskId = parseInt(parentTask.getAttribute("data-task-id"));
+        console.log(taskManager.task);
+        let task = taskManager.getTaskById(taskId);
+        task.status = 'Done';
+        if (task.status === "Done") {
+          event.target.classList.remove("visible");
+          event.target.classList.add("invisible");
+        }
+        console.log(parentTask)
+        console.log('clicked button')
+        taskManager.save();
+        // do something 
+    }
+    if (event.target.classList.contains("delete-button")) {
+      let parentTask = event.target.parentNode;
+      let taskId = parseInt(parentTask.getAttribute("data-task-id"));
+      console.log(taskManager.task);
+      taskManager.deleteTask(taskId);
+    //   task.status = "Delete";
+    //   task.status = "Done";
+    //   if (task.status === "Done") {
+    //     event.target.classList.remove("visible");
+    //     event.target.classList.add("invisible");
+    //   }
+      console.log(parentTask);
+      console.log("clicked button");
+      taskManager.save();
+      // do something
+    }
+    taskManager.deleteTask();
+    taskManager.render();
+    taskManager.load();
+  // "event" here is the event parameter
+});
+// const taskHtml = createTaskHtml("Groceries", "description", "Vishva", "09/27/2021")
--- assets/js/taskManager.js
+++ assets/js/taskManager.js
@@ -1,7 +1,12 @@
-let createTaskHtml = (name, description, assignedTo, dueDate) => {
+const tasksList = document.getElementById("tasksList");
+let createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
     // constructor(name, description, assignedTo, dueDate, status){
-        const html = `
-    <li class="list-group-item">
+      let doneButtonVisibility = 'visible';
+    if (status === 'Done') {
+        doneButtonVisibility = 'invisible';
+    };
+        return `
+    <li class="list-group-item" data-task-id="${id}">
         <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
             <h5>Take out ${description}</h5>
             <span class="badge badge-danger">TODO</span>
@@ -11,19 +16,28 @@ let createTaskHtml = (name, description, assignedTo, dueDate) => {
             <small>Due: ${dueDate}</small>
         </div>
         <p>Take out the trash to the front of the house</p>
+        <button type="button" class="btn    
+        done-button
+        btn-dark ${doneButtonVisibility}">Mark as Done</button>
+        <button type="button" class="btn    
+        delete-button
+        btn-dark ${doneButtonVisibility}">Delete</button>
+
+
     </li>
-`
-        return html;
+`;
+        
 
         
 }
-class taskManager {
+class TaskManager {
   constructor(currentId = 0) {
     this.task = [];{
       this.currentId = currentId;
     }
+  }
     
-    addTask(name, description, assignedTo, dueDate)
+    addTask(name, description, assignedTo, dueDate, status)
     {
       const task = {
         id: this.currentId++, 
@@ -33,12 +47,61 @@ class taskManager {
         dueDate: dueDate,
         status: 'TODO'
       };
-      this.task.push(Task);
+      this.task.push(task);
     
       // this.status = "TODO";
     // }
     // push.this.task();
      }
+
+     deleteTask(taskId){
+     console.log(taskId);
+     for (let i = 0; i < this.task.length; i++) {
+       let task = this.task[i];
+       if (task.id === taskId) {
+         this.task.splice(i, 1);
+       }
+     }
+     }
+     getTaskById(taskId){
+       console.log(taskId);
+       let foundTask;
+       for (let i = 0; i < this.task.length; i++) {
+         let task = this.task[i];
+         if (task.id === taskId) {
+           foundTask = task;
+         } 
+       }
+       return foundTask;
+     }
+     save(){
+      let tasksJson = JSON.stringify(this.task);
+      localStorage.setItem("tasks",tasksJson);
+      let currentId = JSON.stringify(this.currentId);
+      localStorage.setItem("currentId", currentId);
+     }
+
+     load(){
+       let tasksJson = localStorage.getItem("tasks");
+       let currentId = localStorage.getItem("currentId");
+       this.task = JSON.parse(tasksJson);
+       this.currentId = JSON.parse(currentId);
+
+     }
     render()
-  }
+    {
+     let tasksHtmlList = new Array();
+     
+     for(let i=0;i<this.task.length;i++){
+       let task = this.task[i]
+       let taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, task.dueDate, task.status)
+       tasksHtmlList.push(taskHtml)
+     }
+     let tasksHtml = tasksHtmlList.join("\n")
+     console.log(tasksHtml);
+     tasksList.innerHTML = tasksHtml
+    //  let formattedDate = 'date' 
+    }
+  
+
 }
